4125119650c1af0da9920b352576af956147fca2
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-04-25 01:33:13 -0300

    feat: middleware

---
+++ src/middlewares/lanche.middleware.js
@@ -0,0 +1,34 @@
+const  mongoose = require('mongoose');
+
+const validId = (req, res, next) => {
+    const idParam = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(idParam)) {
+        return res.status(400).send({message: 'Id inválido'});
+    }
+    next();
+};
+
+//método para validar o json
+const validObjectBody = (req, res, next) => {
+    const lanche = req.body;
+    
+    if (
+        !lancheEdit ||
+        !lancheEdit.local ||
+        !lancheEdit.localizacao ||
+        !lancheEdit.nome ||
+        !lancheEdit.descricao ||
+        !lancheEdit.preco ||
+        !lancheEdit.foto
+        ) {
+            return res.status(400).send({ message: 'Envie todos os campos da paleta' });
+        };
+
+    next();
+  };
+
+module.exports = {
+    validId,
+    validObjectBody,
+};
--- src/routes/lanches.route.js
+++ src/routes/lanches.route.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
 const controllerLanches = require('../controllers/lanches.controller');
+const { 
+    validId , 
+    validObjectBody,
+} = require('../middlewares/lanche.middleware');
 
 //rotas de listagem
 router.get('/lista-lanches', controllerLanches.findLanchesController);
-router.get('/lista-lanches/:id', controllerLanches.findLancheByIdController);
+router.get('/lista-lanches/:id', validId, controllerLanches.findLancheByIdController);
 
 //rotas para criar, atualizar e deletar um iD
-router.post('/create', controllerLanches.createLancheController);
-router.put('/update/:id', controllerLanches.updateLancheController);
-router.delete('/delete/:id', controllerLanches.deleteLancheController);
+router.post('/create', validObjectBody, controllerLanches.createLancheController);
+router.put('/update/:id', validId, validObjectBody, controllerLanches.updateLancheController);
+router.delete('/delete/:id', validId, controllerLanches.deleteLancheController);
 
 module.exports = router;
